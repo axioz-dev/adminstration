e9942ed79bca6db4acc2fc9f269f15d3e3342142
codeshaine <shainilps05@gmail.com>
2024-04-29 15:44:17 +0530

    correcting port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
-cosnt PORT=process.env.PORT || 3000;  
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 // const collection = connectToDatabase();
